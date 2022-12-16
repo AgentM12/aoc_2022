2f085db8fd77ce54c8840a3597d7d214b2b27518
AgentM <melvinwii@live.com>
2022-12-16 16:00:41 +0100

    Day 11

--- day10.py
+++ day10.py
@@ -1,10 +1,77 @@
 import os
 
+sol2 = "ZRARLFZU" # HARDCODED TEXT RESULT EXTRACTED FROM SCREEN GRAPHIC
+
+cmd_registry = {
+	"noop": (1, 0),
+	"addx": (2, 1)
+}
+
 def part1(inp):
-	pass
+	poi = [20, 60, 100, 140, 180, 220]
+	res = computer_run(inp, poi)
+
+	s = 0
+	for i in range(len(poi)):
+		s += poi[i] * res[i]
+	return s
 
 def part2(inp):
-	pass
+	screen = computer_draw(inp, 40, 6)
+	return f"{sol2}\n" + "\n".join(["".join(line) for line in screen])
+
+def computer_draw(program, w, h):
+	cycles = 0
+	rx = 1
+	screen = []
+	for dh in range(h):
+		screen.append([])
+
+	for command in program:
+		cmdparts = command.split()
+		cmdinfo = cmd_registry[cmdparts[0]]
+
+		for c in range(cmdinfo[0]):
+			tx = cycles % w
+			ty = int(cycles / w)
+			if (ty >= h): return screen
+
+			if tx >= rx - 1 and tx <= rx + 1:
+				screen[ty].append('#')
+			else:
+				screen[ty].append('.')
+			cycles += 1
+
+		if cmdinfo[1] > 0:
+			args = cmdparts[1:]
+			if cmdparts[0] == 'addx' and cmdinfo[1] == 1:
+				rx += int(args[0])
+			else: raise Exception("Currently unsupported")
+	return screen
+
+def computer_run(program, probepoi):
+	store = []
+	nextpoi = 0
+	cycles = 0
+	rx = 1
+
+	for command in program:
+		cmdparts = command.split()
+		cmdinfo = cmd_registry[cmdparts[0]]
+
+		cycles += cmdinfo[0]
+
+		if (cycles >= probepoi[nextpoi]):
+			store.append(rx)
+			nextpoi += 1
+			if nextpoi >= len(probepoi): return store
+
+		if cmdinfo[1] > 0:
+			args = cmdparts[1:]
+			if cmdparts[0] == 'addx' and cmdinfo[1] == 1:
+				rx += int(args[0])
+			else: raise Exception("Currently unsupported")
+
 
 def get_input(fname):
 	inp_file_name = f"{fname}_input.txt"
--- day11.py
+++ day11.py
@@ -1,10 +1,125 @@
 import os
+import math
+
+class Monkey:
+
+	def __init__(self, items, op, cond, ta, fa):
+		self.items = items # items starting with
+		self.op = op # operation to perform
+		self.cond = cond # condition to test item divisible by 'cond'
+		self.ta = ta # reference monkey if True
+		self.fa = fa # reference monkey if False
+		self.inspections = 0
+		self.lcm = None
+
+	def inspect_all(self):
+		while (len(self.items) > 0):
+			self.inspect()
+
+	def inspect(self):
+		self.inspections += 1
+		# next item
+		item = self.items[0]
+		del self.items[0]
+
+		# inspect item
+		item = self.op(item)
+
+		# bored
+		if self.lcm is None:
+			item = int(item / 3)
+		else:
+			item %= self.lcm
+
+		# test
+		if (item % self.cond == 0):
+			throw(item, self.ta)
+		else:
+			throw(item, self.fa)
+
+	def give(self, item):
+		self.items.append(item)
+
+	def __repr__(self):
+		return str(self)
+
+	def __str__(self):
+		return f"\n - Items: {self.items}.\n - Result of op(x) for 1 to 3: {self.op(1)}, {self.op(2)}, {self.op(3)}\n - if (i % {self.cond})\n   - then {self.ta}\n   - else {self.fa}\n"
+
+monkeys = None
+
+def throw(item, monkey_id):
+	global monkeys
+	monkeys[monkey_id].give(item)
 
 def part1(inp):
-	pass
+	global monkeys
+	init_monkeys(inp)
+	
+	return play(20)
 
 def part2(inp):
-	pass
+	global monkeys
+	init_monkeys(inp, True)
+	
+	return play(10000)
+
+def play(rounds):
+	for r in range(rounds):
+		for m in monkeys:
+			m.inspect_all()
+
+	best2 = [0, 0]
+	for m in monkeys:
+		it = m.inspections
+		for bi in range(len(best2)):
+			if it > best2[bi]:
+				it, best2[bi] = best2[bi], it
+	return best2[0] * best2[1]
+
+def init_monkeys(inp, disable_div=False):
+	global monkeys
+	monkeys = []
+	
+	items, op, cond, ta, fa = None, None, None, None, None
+
+	i = 0
+	for line in inp:
+		i += 1
+		if i % 7 == 1:
+			pass
+		elif i % 7 == 2:
+			its = line.strip().split(":")[1].split(',')
+			items = [int(it.strip()) for it in its]
+		elif i % 7 == 3:
+			sop = line.strip().split(":")[1].strip()
+			_, _, _, o, b = sop.split()
+
+			if b == "old" and o == '*':
+				op = lambda x: x * x
+			elif o == '+':
+				op = lambda x, b=b: x + int(b)
+			elif o == '*':
+				op = lambda x, b=b: x * int(b)
+			else:
+				raise Exception(f"Unrecognized operation: {line}")
+		elif i % 7 == 4:
+			cond = int(line.strip().split()[3])
+		elif i % 7 == 5:
+			ta = int(line.strip().split()[5])
+		elif i % 7 == 6:
+			fa = int(line.strip().split()[5])
+		elif i % 7 == 0:
+			monkeys.append(Monkey(items, op, cond, ta, fa))
+	monkeys.append(Monkey(items, op, cond, ta, fa))
+
+	if disable_div:
+		f = 1
+		for m in monkeys:
+			f *= m.cond
+		lcm = f // math.gcd(*[m.cond for m in monkeys])
+		for m in monkeys:
+			m.lcm = lcm
 
 def get_input(fname):
 	inp_file_name = f"{fname}_input.txt"
